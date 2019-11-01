9379edd00152401d76193623728a0ff3026f93ad
Eugene Malchenko <nijoelite@gmail.com>
2019-11-02 02:12:15 +0300

    Init

---
+++ src/redux/Reducer.ts
@@ -0,0 +1,11 @@
+const initialState = {
+  cartItems: [],
+  showCartDialog: false,
+  showMenu: true,
+  checkedOutItems: [],
+  loggedInUser: null,
+};
+
+const rootReducer = (state = initialState, action) => state;
+
+export default rootReducer;
