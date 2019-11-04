5984d160b470d70c7d2db9c570e65fcfc0b424b9
Eugene Malchenko <nijoelite@gmail.com>
2019-11-05 00:24:41 +0300

    Add mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React, {Component} from 'react';
+import React, {Component, FC} from 'react';
 import {Header} from './components/Header/Header.component';
 import { Game } from './models/interfaces';
 import { Catalog } from './components/Catalog/Catalog.component';
 import { Container } from './ui-kit/Container/Container.component';
 import styled from '@emotion/styled/macro';
 import { mediaMd } from './utils/css.utils';
+import { Carousel } from './ui-kit/Carousel/Carousel.component';
 
 const mockData: Game = {
   name: 'Game Of Year',
@@ -20,16 +21,43 @@ const Page = styled.div`
   padding-top: 65px;
 
   ${mediaMd} {
-    padding-top: 30px;
+    padding-top: 0;
   }
 `;
+const mockCarousel = ['https://cdn.gabestore.ru/category/yZX_l6hOvLAz_3TnSUk5jLr03hcObcAB.jpg','https://cdn.gabestore.ru/category/yZX_l6hOvLAz_3TnSUk5jLr03hcObcAB.jpg','https://cdn.gabestore.ru/category/yZX_l6hOvLAz_3TnSUk5jLr03hcObcAB.jpg', 'https://cdn.gabestore.ru/category/0qGrBrlPS12ri3PW2DayeHhT2GmNPhT4.jpg'];
+
+const CarouselStyled = styled(Carousel)`
+  width: 100%;
+  height: 500px;
+`;
+
+const Background = styled('div')<{imgUrl: string}>`
+  background-image: url(${({imgUrl}): string => imgUrl});
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const Item: FC = () => <div>ghderfhsetrjsj</div>;
+
 
 export default class App extends Component {
+
   render() {
+    const items = mockCarousel.map(el => {
+      return {
+        item: <Item/>,
+        background: <Background imgUrl={el}/>,
+      }
+     });
+
     return (
       <React.Fragment>
         <Header></Header>
         <Page>
+          <CarouselStyled items={items} options={{delay: 8500}} />
+
           <Container>
             <Catalog games={games}/>
           </Container>
